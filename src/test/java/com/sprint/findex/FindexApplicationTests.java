a0626331f37ce1ded4e052492f7a645acbebb0fb
dustle <pwdustle@gmail.com>
2026-03-11 19:37:16 +0900

    chore: initialize Spring Boot project

---
+++ src/test/java/com/sprint/findex/FindexApplicationTests.java
@@ -0,0 +1,15 @@
+package com.sprint.findex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class FindexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
