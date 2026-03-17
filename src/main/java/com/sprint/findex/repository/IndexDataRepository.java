4194818acb14a041a505d89ab2a0b8c0506b9fc4
ParkSungGuk <51737908+PSG-00@users.noreply.github.com>
2026-03-17 14:33:23 +0900

    feat: 지수 데이터 목록 조회 구현 및 컨트롤러 문서화 (#73)
    * merge 전 커밋
    
    * feat: IndexData 생성
    
    * feat: IndexData Controller 생성 및 post 핸들러 메서드 구현
    
    * style: 들여쓰기 리포맷
    
    * feat: IndexData Update 구현
    
    * fix:style: IndexDateService 수정
    
    * 들여쓰기 및 코드 라인 정리
    
    * feat: IndexData delete  구현
    
    * feat: IndexData Export 기능 구현
    
    * style: @ResponseStatus 어노테이션 삭제 및 Controller 핸들러 메서드명 도메인 추가
    
    * feat: IndexData 커서 기반 페이지네이션 구현
    
    * style: PageResponse를 사용하도록 변경
    
    * feat: IndexDataController API 문서화 작성
    
    * IndexDataExport 문서화 과정에서 가독성을 위해 requestDto 작성 및 사용
    
    * fix: merge과정에서 살아나ë²버린 코드 삭제
    
    * style: 조회에서 사용되는 쿼리 메서드명 변경

--- src/main/java/com/sprint/findex/repository/IndexDataRepository.java
+++ src/main/java/com/sprint/findex/repository/IndexDataRepository.java
@@ -1,17 +1,18 @@
 package com.sprint.findex.repository;
 
 import com.sprint.findex.entity.IndexData;
+import com.sprint.findex.repository.dsl.IndexDataCustomRepository;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 
-public interface IndexDataRepository extends JpaRepository<IndexData, UUID> {
+public interface IndexDataRepository extends JpaRepository<IndexData, UUID>,
+        IndexDataCustomRepository {
 
     boolean existsByIndexInfo_IdAndBaseDate(UUID indexInfoId, LocalDate baseDate);
 
@@ -24,4 +25,6 @@ public interface IndexDataRepository extends JpaRepository<IndexData, UUID> {
             @Param("startDate") LocalDate startDate,
             @Param("endDate") LocalDate endDate,
             Sort sort);
+
+    long countByIndexInfoId(UUID indexInfoId);
 }
